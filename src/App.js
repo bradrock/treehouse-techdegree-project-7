055bea82acd3edeef10c50621bd77d71616c8027
Brad Rock <32444574+bradrock@users.noreply.github.com>
2020-06-11 03:55:46 -0700

    Make search form functional

--- src/App.js
+++ src/App.js
@@ -71,14 +71,57 @@ function Photo(props) {
 
 
 class Form extends React.Component {
-  constructor() {
-    super();
+
+/*
+ source for setting up search form functionality: https://www.freecodecamp.org/forum/t/getting-an-input-value-when-form-is-submitted-react/161870/3
+*/
+
+  constructor(props) {
+    super(props);
+    this.searchRef = React.createRef();
+    this.aRef = React.createRef();
+    this.updateInputValue = React.createRef();
+    this.state = {inputValue: ''};
+  
+    
     //this.state = {color: "red"};
   }
+
+ 
+
+/*
+  updateInputValue => (evt) => {
+    this.setState({
+      inputValue: evt.target.value
+    });
+  }
+*/
+
+  
+  
+  componentDidMount(){
+    //this.aRef.href= '/search/' +  this.state.inputValue;
+
+
+  }
+  
+
+  handleChange = (e) => {
+        this.setState({
+            inputValue: e.target.value
+        })
+    }
+    
+  onSubmit = (e) => {
+       e.preventDefault();
+       window.location.href = "/search/" + this.state.inputValue;
+    }
+
+
   render() {
     return (<form className="search-form">
-    <input type="search" name="search" placeholder="Search" required/>
-    <button type="submit" className="search-button">
+    <input value={this.state.inputValue} onChange={e => this.handleChange(e)} type="search" name="search" placeholder="Search" required/>
+    <button onClick={(e) => this.onSubmit(e)} type="submit" className="search-button">
       <svg fill="#fff" height="24" viewBox="0 0 23 23" width="24" xmlns="http://www.w3.org/2000/svg">
         <path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"/>
         <path d="M0 0h24v24H0z" fill="none"/>
@@ -86,6 +129,48 @@ class Form extends React.Component {
     </button>
   </form>);
   }
+
+  
+}
+
+/*
+render() {
+  return (<form className="search-form">
+  <input type="search" name="search" placeholder="Search" required/>
+  <button type="submit" className="search-button">
+    <svg fill="#fff" height="24" viewBox="0 0 23 23" width="24" xmlns="http://www.w3.org/2000/svg">
+      <path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"/>
+      <path d="M0 0h24v24H0z" fill="none"/>
+    </svg>
+  </button>
+</form>);
+*/
+
+
+class Button extends React.Component {
+
+  constructor(props) {
+    super(props);
+    //this.state = {isToggleOn: true};
+
+    // This binding is necessary to make `this` work in the callback
+    //this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState(state => ({
+      isToggleOn: !state.isToggleOn
+    }));
+  }
+
+  render() {
+    return (
+      <button onClick={this.handleClick}>
+        {this.state.isToggleOn ? 'ON' : 'OFF'}
+      </button>
+    );
+  }
+
 }
 
 
