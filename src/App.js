f87501f9d2ca9bb6d360458b9642869623d6be11
Brad Rock <32444574+bradrock@users.noreply.github.com>
2020-06-13 06:28:24 -0700

    updated font in index.css

--- src/App.js
+++ src/App.js
@@ -1,30 +1,5 @@
 import React from 'react';
 import './App.css';
-<<<<<<< HEAD
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
-=======
 import ReactDOM from 'react-dom';
 import {Router, Route} from 'react-router';
 import {BrowserRouter, Switch} from 'react-router-dom';
@@ -208,4 +183,3 @@ export default class App extends React.Component {
     );
   }
 }
->>>>>>> 88c3ac7... added comments
